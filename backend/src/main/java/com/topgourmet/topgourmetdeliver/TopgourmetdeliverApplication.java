446b54f8ac09c5da76ee9b8ffef1f969b5746d0c
burkepaula <paulanepo.burke@gmail.com>
2022-11-26 17:28:44 -0300

    Project created

---
+++ backend/src/main/java/com/topgourmet/topgourmetdeliver/TopgourmetdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.topgourmet.topgourmetdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopgourmetdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopgourmetdeliverApplication.class, args);
+	}
+
+}
